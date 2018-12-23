b157fba4369cde4effb8090efc18546f856d22b9
Zhicheng WANG <zcwang@thoughtworks.com>
2018-12-23 16:26:32 +0800

    chore: 发布时不再自动构建

--- translator/deploy-cn.sh
+++ translator/deploy-cn.sh
@@ -3,6 +3,7 @@
 set -x
 set -e
 
+npx gulp material-examples:build-release:clean
 yarn docs
 
 cd `dirname $0`
@@ -12,7 +13,7 @@ npm start
 cd -
 
 rm -fr ./tmp/
-./scripts/deploy/publish-docs-content.sh
+./scripts/deploy/publish-docs-content.sh --no-build
 sleep 5
 cd ../material.github.io
 ./translator/deploy.sh
